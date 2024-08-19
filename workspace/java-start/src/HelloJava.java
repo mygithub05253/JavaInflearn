a4b0aac7380f9be3d8d4fe804597a1c9dbbe1828
mygithub05253 <kik328288@gmail.com>
2024-08-19 14:17:39 +0900

    자바 입문 수업 코드1

---
+++ workspace/java-start/src/HelloJava.java
@@ -0,0 +1,8 @@
+// 클래스의 이름은 파일명과 동일해야 함
+public class HelloJava {
+    // main 메서드
+    public static void main(String[] args) {
+        // 시스템에 값을 출력하는 기능
+        System.out.println("hello java");
+    }
+}
